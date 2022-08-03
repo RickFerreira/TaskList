d918b372f873d20d18b9e9fffc632ff77547b0ec
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-08-03 10:37:58 -0300

    primeiro commit
    classes Tarefa e CurtoPrazo finalizada

---
+++ Tasklist/CurtoPrazo.h
@@ -0,0 +1,24 @@
+#ifndef CURTO_H_
+#define CURTO_H_
+
+#include "Tarefa.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CurtoPrazo : public Tarefa{
+    //ATTRIBUTES
+    private:
+        string horario;
+    //CONSTRUCTOR
+    public:
+        CurtoPrazo(string nome, string descricao, string tipo, string horario);
+
+    //METHODS
+        string getHorario();
+        void setHorario(string horario);
+
+};
+#endif
